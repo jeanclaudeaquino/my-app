10b801799a269cd9936ebca389b726e3c455242d
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2024-10-06 20:01:49 +1300

    chore: reject deployment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const HomeScreen = () => {
     <div style={styles.container}>
       <h1 style={styles.title}>JC's Demo app for authorized deployment</h1>
       <p style={styles.description}>
-        Re-deployment trigger
+       Will not deploy because declined
       </p>
       <button style={styles.button} onClick={() => alert('Button clicked!')}>
         Get Started
