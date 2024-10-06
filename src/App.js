0d4fd517c912488f0e9ea8e51c6453a4a55a076e
jeanclaudeaquino <jeanclaudeaquino@gmail.com>
2024-10-06 23:12:17 +1300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const HomeScreen = () => {
     <div style={styles.container}>
       <h1 style={styles.title}>JC's Demo app for authorized deployment (Chnaged to approved)</h1>
       <p style={styles.description}>
-      Will deploy since will approved
+       On my machine
       </p>
       <button style={styles.button} onClick={() => alert('Button clicked!')}>
         Get Started
